d8218c9bf5d08b4ce57abc75a2488ac67d1b8af6
thennek0 <kennethbecaro0@gmail.com>
2024-03-25 16:45:24 +0800

    add menu bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '1.5xl': ['1.35rem', { lineHeight: '1.80rem' }],
+      },
+    },
   },
   plugins: [],
 }
